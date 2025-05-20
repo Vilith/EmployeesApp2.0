2799485dd67426f2b456024dc15ed26301cfa153
Vilith <vilith44@gmail.com>
2025-05-20 11:56:33 +0200

    First joint commit

---
+++ EmployeesApp.Web/Views/Employees/CreateVM.cs
@@ -0,0 +1,22 @@
+﻿using EmployeesApp.Web.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesApp.Web.Views.Employees
+{
+    public class CreateVM
+    {
+
+        [Required(ErrorMessage = "You must specify a name")]
+        [SuspiciousPerson("Pontus Wittenmark", ErrorMessage = "Suspicious person detected!")]
+        public string Name { get; set; }
+
+        [Display(Name = "E-mail")]
+        [EmailAddress(ErrorMessage = "Invalid e-mail address")]
+        [Required(ErrorMessage = "You must specify an e-mail address")]
+        public string Email { get; set; } = null!;
+
+        [Display(Name = "E-mail (again)")]
+        [Range(0,4)]
+        public int BotCheck { get; set; } = 4;
+    }
+}
---
+++ EmployeesApp.Web/Views/Employees/IndexVM.cs
@@ -0,0 +1,17 @@
+﻿namespace EmployeesApp.Web.Views.Employees
+{
+    public class IndexVM
+    {
+        public required EmployeeItemVM[] Employees { get; set; }
+
+    }
+
+    public class EmployeeItemVM
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+
+        public bool ShowAsHighlighted { get; set; } = false;
+    }
+}
